e7083ec7cd8647af1566d1957ed97598428ca7d0
beatriz campos leon <bcampos@zeta40.aulas.gsyc.es>
2015-09-28 16:19:18 +0200

    calcoohija.py Mejorado - Importando clase Calculadora

--- calcoohija.py
+++ calcoohija.py
@@ -2,18 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import sys
+import calcoo
 
 
-class Calculadora():
-
-    def suma(self, num1, num2):
-        return num1 + num2
-
-    def resta(self, num1, num2):
-        return num1 - num2
-
-
-class CalculadoraHija(Calculadora):
+class CalculadoraHija(calcoo.Calculadora):
     def multiplicar(self, num1, num2):
         return num1 * num2
 

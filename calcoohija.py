ebc3b50268c7f7d77fe8a7ea506bc7ccb83cf30f
beatriz campos leon <bcampos@zeta40.aulas.gsyc.es>
2015-09-28 15:53:25 +0200

    calcoohija.py Finalizado - Incluido multiplicacion y division

---
+++ calcoohija.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+
+class Calculadora():
+
+    def suma(self, num1, num2):
+        return num1 + num2
+
+    def resta(self, num1, num2):
+        return num1 - num2
+
+
+class CalculadoraHija(Calculadora):
+    def multiplicar(self, num1, num2):
+        return num1 * num2
+
+    def dividir(self, num1, num2):
+        try:
+            return num1 / num2
+        except ZeroDivisionError:
+            sys.exit("Error: Division by zero is not allowed")
+
+
+if __name__ == "__main__":
+    try:
+        operando1 = int(sys.argv[1])
+        operando2 = int(sys.argv[3])
+    except ValueError:
+        sys.exit("Error: Non numerical parameters")
+
+    calculadora = CalculadoraHija()
+
+    if sys.argv[2] == "suma":
+        result = calculadora.suma(operando1, operando2)
+    elif sys.argv[2] == "resta":
+        result = calculadora.resta(operando1, operando2)
+    elif sys.argv[2] == "multiplica":
+        result = calculadora.multiplicar(operando1, operando2)
+    elif sys.argv[2] == "divide":
+        result = calculadora.dividir(operando1, operando2)
+    else:
+        sys.exit('Sólo puede ser sumar,restar, multiplicar o dividir.')
+
+    print(result)

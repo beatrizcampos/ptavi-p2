2c766705b216435cbfd8194d6fb8cca185573ace
beatrizcampos <beatrizcl.1404@gmail.com>
2015-10-03 23:56:03 +0200

    calcplus.py realizada resta, multiplicacion y division

--- calcplus.py
+++ calcplus.py
@@ -7,23 +7,22 @@ import calcoohija
 
 
 if __name__ == "__main__":
-    fich = open(sys.argv[1],'r')
-    linea = fich.readlines() 
-    num_lineas = len(linea) ##numero de lineas que hay
+    fich = open(sys.argv[1], 'r')
+    linea = fich.readlines()
+    num_lineas = len(linea)##numero de lineas que hay
     print('numero de lineas que hay en texto', num_lineas)
     elementos = linea[0].split(',')##elementos que hay en linea
-    print('elementos', elementos) 
+    print('elementos', elementos)
     print('numero de elementos', len(elementos)) ##numero elementos en linea
-    print(elementos[0])  ## imprime el primer elemento
-    print(elementos[1])  ##imprime el segundo elemento
+    print(elementos[0])## imprime el primer elemento
+    print(elementos[1])##imprime el segundo elemento
     print(elementos[1:])
-
     calculadora = calcoohija.CalculadoraHija()
     i = 0
-    while i < num_lineas:   #Vamos cogiendo las lineas hasta llegar al final
+    while i < num_lineas:#Vamos cogiendo las lineas hasta llegar al final
        elementos = linea[i].split(',')
-       operacion = elementos[0] #La operacion es el primer elemento
-       operadores = elementos[1:] #El resto de elementos son operadores
+       operacion = elementos[0]#La operacion es el primer elemento
+       operadores = elementos[1:]#El resto de elementos son operadores
        operadores[-1] = operadores[-1][:-1]
        print(operacion)
        print(operadores)
@@ -35,13 +34,20 @@ if __name__ == "__main__":
                operando = int(operadores[j]) 
                resultado = calculadora.suma(resultado, operando)
                j = j +1
-
-
+       if operacion == "resta":
+           while j < len(operadores):
+               operando = int(operadores[j]) 
+               resultado = calculadora.resta(resultado, operando)
+               j = j +1
+       if operacion == "multiplica":
+           while j < len(operadores):
+               operando = int(operadores[j]) 
+               resultado = calculadora.multiplicar(resultado, operando)
+               j = j +1
+       if operacion == "divide":
+           while j < len(operadores):
+               operando = int(operadores[j]) 
+               resultado = calculadora.dividir(resultado, operando)
+               j = j +1
+       print(resultado)
        i = i + 1
-        
-        
-
-    
-
-
-

dae25c239939e8e6c5e75af243450766ae60b4f2
beatriz campos leon <bcampos@zeta40.aulas.gsyc.es>
2015-10-05 16:07:37 +0200

    calcplus.py - Mejora

--- calcplus.py
+++ calcplus.py
@@ -6,16 +6,27 @@ import calcoo
 import calcoohija
 
 
+def operaciones(operadores, resultado, operacion):
+    for operando in operadores[1:]:
+        operando = int(operando)
+        if operacion == "suma":
+            resultado = calculadora.suma(resultado, operando)
+        elif operacion == "resta":
+            resultado = calculadora.resta(resultado, operando)
+        elif operacion == "multiplica":
+            resultado = calculadora.multiplicar(resultado, operando)
+        elif operacion == "divide":
+            resultado = calculadora.dividir(resultado, operando)
+        else:
+            sys.exit('Sólo puede ser sumar,restar, multiplicar o dividir.')
+    return resultado
+
 if __name__ == "__main__":
     fich = open(sys.argv[1], 'r')
-    linea = fich.readlines()
-    #numero de lineas que hay
-    num_lineas = len(linea)
+    lineas = fich.readlines()
     calculadora = calcoohija.CalculadoraHija()
-    i = 0
-    #Vamos cogiendo las lineas hasta llegar al final
-    while i < num_lineas:
-        elementos = linea[i].split(',')
+    for linea in lineas:
+        elementos = linea.split(',')
         #La operacion es el primer elemento
         operacion = elementos[0]
         #El resto de elementos son operadores
@@ -23,28 +34,5 @@ if __name__ == "__main__":
         operadores[-1] = operadores[-1][:-1]
         #Hacemos que resultado sea el primer elemento y "operando" el resto
         resultado = int(operadores[0])
-        j = 1
-        if operacion == "suma":
-            while j < len(operadores):
-                operando = int(operadores[j])
-                resultado = calculadora.suma(resultado, operando)
-                j = j + 1
-        elif operacion == "resta":
-            while j < len(operadores):
-                operando = int(operadores[j])
-                resultado = calculadora.resta(resultado, operando)
-                j = j + 1
-        elif operacion == "multiplica":
-            while j < len(operadores):
-                operando = int(operadores[j])
-                resultado = calculadora.multiplicar(resultado, operando)
-                j = j + 1
-        elif operacion == "divide":
-            while j < len(operadores):
-                operando = int(operadores[j])
-                resultado = calculadora.dividir(resultado, operando)
-                j = j + 1
-        else:
-            sys.exit('Sólo puede ser sumar,restar, multiplicar o dividir.')
+        resultado = operaciones(operadores, resultado, operacion)
         print(resultado)
-        i = i + 1

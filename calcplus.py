9e74d7680db629a1ad99a44e4300d3b2357bc0c3
beatriz campos leon <bcampos@zeta40.aulas.gsyc.es>
2015-10-05 16:15:49 +0200

    calcplus.py - Mejora

--- calcplus.py
+++ calcplus.py
@@ -6,25 +6,21 @@ import calcoo
 import calcoohija
 
 
-def operaciones(operadores, resultado, operacion):
-    for operando in operadores[1:]:
-        operando = int(operando)
-        if operacion == "suma":
-            resultado = calculadora.suma(resultado, operando)
-        elif operacion == "resta":
-            resultado = calculadora.resta(resultado, operando)
-        elif operacion == "multiplica":
-            resultado = calculadora.multiplicar(resultado, operando)
-        elif operacion == "divide":
-            resultado = calculadora.dividir(resultado, operando)
-        else:
-            sys.exit('Sólo puede ser sumar,restar, multiplicar o dividir.')
+def operaciones(operadores, resultado, operacion, diccionario):
+    try:
+        funcion = dicc[operacion]
+        for operando in operandos[1:]:
+            operando = int(operando)
+            result = funcion(result, operando)
+    except:
+        sys.exit('Sólo puede ser sumar,restar, multiplicar o dividir.')
     return resultado
 
 if __name__ == "__main__":
     fich = open(sys.argv[1], 'r')
     lineas = fich.readlines()
     calculadora = calcoohija.CalculadoraHija()
+    diccionario = {"suma": calculadora.suma, "resta": calculadora.resta, "multiplica": calculadora.multiplicar, "divide": calculadora.dividir}
     for linea in lineas:
         elementos = linea.split(',')
         #La operacion es el primer elemento
@@ -34,5 +30,5 @@ if __name__ == "__main__":
         operadores[-1] = operadores[-1][:-1]
         #Hacemos que resultado sea el primer elemento y "operando" el resto
         resultado = int(operadores[0])
-        resultado = operaciones(operadores, resultado, operacion)
+        resultado = operaciones(operadores, resultado, operacion, diccionario)
         print(resultado)
